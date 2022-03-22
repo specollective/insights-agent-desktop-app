01eb2fa7d80c4f77df5ebf626912208332416dfb
Matt Moore <Matt.M.Moore13@outlook.com>
2022-03-22 16:51:51 -0400

    replaced all mainWindow.fileLoad paths with MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY

--- src/index.js
+++ src/index.js
@@ -15,12 +15,12 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     }
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadURL(MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY);
 
   // Open the DevTools.
   if (process.env['DEVELOPMENT']) {
