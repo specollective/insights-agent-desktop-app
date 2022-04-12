42beb12cde299e917085a8d538d2d6100bb27d19
Joe Torreggiani <joetorreggiani@gmail.com>
2022-04-11 19:17:11 -0700

    Basic IPC logic.

--- src/preload.js
+++ src/preload.js
@@ -1,3 +1,5 @@
+const { ipcRenderer } = require('electron')
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
@@ -7,4 +9,17 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const dependency of ['chrome', 'node', 'electron']) {
     replaceText(`${dependency}-version`, process.versions[dependency])
   }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('test-button').onclick = () => {
+    ipcRenderer.send('status-check-request', 'example message');
+    document.getElementById('status').innerText = 'Waiting..';
+  }
+
+  // Receive reply from elecron
+  // See file main.js on line 37
+  ipcRenderer.on('status-check-reply', (event, response) => {
+    document.getElementById('status').innerText = response
+  });
 })
