79fff2a6d60d2ed7b268b50714500b8c7a895596
Joe Torreggiani <joetorreggiani@gmail.com>
2022-11-06 21:07:17 -0500

    Make API url env aware.

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,7 +1,8 @@
-const { app } = require('electron')
+const { app } = require('electron');
 
-// TODO: Use process.env['BACKEND_API_URL']
-const BASE_URL = 'https://insights-agent-api.specollective.org'
+const BASE_URL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:3333'
+  : 'https://insights-agent-api.specollective.org';
 
 const DEFAULT_OPTIONS = {
   mode: 'cors',
