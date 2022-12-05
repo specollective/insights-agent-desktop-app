ce4654d59da344bb1cf2b8b1564da74c2c70caf1
Yoline <92892101+YolineB@users.noreply.github.com>
2022-12-05 15:38:23 -0800

    feat:Add window close functionality

--- src/renderer/components/pages/DashboardPage.jsx
+++ src/renderer/components/pages/DashboardPage.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 
 function DashboardPage() {
   return (
-    <div className="page dashboard">
-      <p>
-        The Insights Agent is now running. By closing this window, you will not pause the study.
-      </p>
+    <>
+      <div className="page dashboard">
+        <p>
+          The Insights Agent is now running. By closing this window, you will not pause the study.
+        </p>
 
-      <p>
-        If you are having trouble or have any questions, please contact tech4all@buildjustly.org.
-      </p>
+        <p>
+          If you are having trouble or have any questions, please contact tech4all@buildjustly.org.
+        </p>
 
-      <p>
-        If you would like to opt out of this study, click the Pause button in the taskbar and follow instructions.
-      </p>
-    </div>
+        <p>
+          If you would like to opt out of this study, click the Pause button in the taskbar and follow instructions.
+        </p>
+      </div>
+
+      <button className="close" onClick={window.close}>Close</button>
+    </>
   )
 }
 
