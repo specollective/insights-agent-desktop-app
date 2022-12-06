e0623ce236a42e2a82ce77808fdf77ab2073feb3
Saige Cross <saigecrossdev@gmail.com>
2022-12-06 12:42:27 -0600

    Added updated forge file to fix tests.

--- forge.config.js
+++ forge.config.js
@@ -1,78 +1,78 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   packagerConfig: {
     osxSign: {
-      identity: process.env['APPLE_IDENTITY'],
+      identity: process.env["APPLE_IDENTITY"],
       hardenedRuntime: true,
-      'gatekeeper-assess': false,
-      entitlements: 'entitlements.plist',
-      'entitlements-inherit': 'entitlements.plist',
-      'signature-flags': 'library',
+      "gatekeeper-assess": false,
+      entitlements: "entitlements.plist",
+      "entitlements-inherit": "entitlements.plist",
+      "signature-flags": "library",
     },
     osxNotarize: {
-      appleId: process.env['APPLE_ID'],
-      appleIdPassword: process.env['APPLE_ID_PASS'],
+      appleId: process.env["APPLE_ID"],
+      appleIdPassword: process.env["APPLE_ID_PASS"],
     },
-    executableName: 'insights-agent-desktop-app',
+    executableName: "insights-agent-desktop-app",
     extraResource: [
-      './scripts/mac-data-tracker/program.scpt',
-      './scripts/windows-data-tracker/build',
+      "./scripts/mac-data-tracker/program.scpt",
+      "./scripts/windows-data-tracker/build",
     ],
   },
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {
-        name: 'insights-agent-desktop-app',
+        name: "insights-agent-desktop-app",
         // NOTE: We are manually signing the executable right now.
-        certificateFile: process.env['CERTIFICATE_WINDOWS_PFX'],
-        certificatePassword: process.env['WINDOWS_CERTIFICATE_PASSWORD'],
+        certificateFile: process.env["CERTIFICATE_WINDOWS_PFX"],
+        certificatePassword: process.env["WINDOWS_CERTIFICATE_PASSWORD"],
       },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
   publishers: [
     {
-      name: '@electron-forge/publisher-github',
+      name: "@electron-forge/publisher-github",
       config: {
         repository: {
-          owner: 'specollective',
-          name: 'insights-agent-desktop-app',
+          owner: "specollective",
+          name: "insights-agent-desktop-app",
         },
       },
     },
   ],
   plugins: [
-    {
-      name: '@electron-forge/plugin-webpack',
-      config: {
-        mainConfig: './webpack.main.config.js',
-        renderer: {
-          config: './webpack.renderer.config.js',
-          entryPoints: [
-            {
-              html: './src/renderer/index.html',
-              js: './src/renderer.js',
-              name: 'main_window',
-              preload: {
-                js: './src/preload.js',
+      {
+        name: "@electron-forge/plugin-webpack",
+        config: {
+          mainConfig: "./webpack.main.config.js",
+          renderer: {
+            config: "./webpack.renderer.config.js",
+            entryPoints: [
+              {
+                html: "./src/renderer/index.html",
+                js: "./src/renderer.js",
+                name: "main_window",
+                preload: {
+                  js: "./src/preload.js",
+                },
               },
-            },
-          ],
+            ],
+          },
         },
       },
-    },
   ],
-}
+};
