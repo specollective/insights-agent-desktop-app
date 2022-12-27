dcd548372f9ff2cfb4ccdc26fbb1a909fc86c1c7
Joe Torreggiani <joetorreggiani@gmail.com>
2022-12-26 22:12:44 -0500

    Add documentation.

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -4,8 +4,10 @@ const BASE_URL = process.env.DEVELOPMENT === 'true'
   ? 'https://localhost:8000'
   : 'https://insights-agent-api.specollective.org';
 
-// TODO: Handle ingestion gracefully in development.
-const BASE_INGESTION_URL = process.env.INGESTION_URL || 'http://localhost:6000';
+// TODO: Handle ingestion gracefully in development.  
+const BASE_INGESTION_URL = process.env.DEVELOPMENT === 'true'
+  ? 'http://localhost:6000'
+  : process.env.INGESTION_URL;
 
 const DEFAULT_OPTIONS = {
   mode: 'cors',
