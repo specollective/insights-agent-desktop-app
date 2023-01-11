ac4c4d2cdb0d7b03e4f880a01530d6aed22c5399
Joe Torreggiani <joetorreggiani@gmail.com>
2023-01-11 15:48:11 -0500

    Add translations.

---
+++ src/translations.js
@@ -0,0 +1,18 @@
+const translations = {
+  en: {
+    translation: {
+      dashboard: {
+        welcome: "The Insights Agent is now running. By closing this window, you will not pause the study."
+      }
+    }
+  },
+  en: {
+    translation: {
+      dashboard: {
+        welcome: "The Insights Agent is now running. By closing this window, you will not pause the study."
+      }
+    }
+  },
+};
+
+export default translations;
