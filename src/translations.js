b4cdab6c51f5c562a8d4283bfb21a98819f01007
Yoline <92892101+YolineB@users.noreply.github.com>
2023-01-24 15:37:41 -0800

    Add translation keys, Dashboard

--- src/translations.js
+++ src/translations.js
@@ -2,7 +2,10 @@ const translations = {
   en: {
     translation: {
       dashboard: {
-        welcome: "The Insights Agent is now running. By closing this window, you will not pause the study."
+        welcome: "The Insights Agent is now running. By closing this window, you will not pause the study.",
+        contact: "If you are having trouble or have any questions, please contact tech4all@buildjustly.org.",
+        optout: "If you would like to opt out of this study, click the Pause button in the taskbar and follow instructions.",
+        close:"Close"
       },
       menu: {
         dashboard: 'Dashboard',
@@ -14,7 +17,10 @@ const translations = {
   es: {
     translation: {
       dashboard: {
-        welcome: "La aplicación ahora se está ejecutando. Al cerrar esta ventana, no pausará el estudio."
+        welcome: "La aplicación ahora se está ejecutando. Al cerrar esta ventana, no pausará el estudio.",
+        contact: "If you are having trouble or have any questions, please contact tech4all@buildjustly.org.",
+        optout: "If you would like to opt out of this study, click the Pause button in the taskbar and follow instructions.",
+        close:"Close",
       },
       menu: {
         dashboard: 'Panel',
