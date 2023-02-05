ebb76386f864b1f910defb716ba2e9150a1f8a68
Joe Torreggiani <joetorreggiani@gmail.com>
2023-02-04 23:00:16 -0800

    Handle test env.

--- src/constants/environments.js
+++ src/constants/environments.js
@@ -1,2 +1,4 @@
-export const DEVELOPMENT_MODE = process.env.DEVELOPMENT === 'true';
 export const NODE_ENV = process.env.NODE_ENV;
+export const DEVELOPMENT_MODE = process.env.DEVELOPMENT === 'true' ||
+  NODE_ENV === 'development' ||
+  NODE_ENV === 'test'
