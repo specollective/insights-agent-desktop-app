d06d562613feab226d665efdded47424282be59d
Yoline <92892101+YolineB@users.noreply.github.com>
2023-02-05 00:37:48 -0800

    Issue 19: Implement Desktop Spanish Translations (#66)
    * Add translation keys, Dashboard
    * Add translation keys, Confirmation
    * Add translation keys, Send Access and Setup
    * Add spanish translations
    * Add test local context.
    * Add back translations for native menu.
    ---------
    Co-authored-by: Joe Torreggiani <joetorreggiani@gmail.com>
    Co-authored-by: Yoline <92892101+YolineB@users.noreply.github.com>

--- src/renderer/components/pages/SetupActivityTrackingPage.jsx
+++ src/renderer/components/pages/SetupActivityTrackingPage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
 
 const STATUSES = {
   PENDING: 'PENDING',
@@ -8,20 +10,22 @@ const STATUSES = {
 }
 
 function ActivityTrackingStatusInstructions ({ status, error }) {
+  const { t } = useTranslation()
+
   if (status === STATUSES.PENDING) {
     return (
       <div>
-        <h1 className='py-2 text-2xl font-bold'>Before you click Start, please read this.</h1>
+        <h1 className='py-2 text-2xl font-bold'>{t('setup.before')}</h1>
         <ul className='list-disc py-4 pl-10 text-lg '>
-          <li>The purpose of this study is to collect data on how you use your computer.</li>
-          <li>The data will be anonymized to ensure your privacy.</li>
-          <li>You will be paid for taking part in this study.</li>
+          <li>{t('setup.purpose')}</li>
+          <li>{t('setup.privacy')}</li>
+          <li>{t('setup.paid')}</li>
         </ul>
         <p className='py-2 text-lg'>
-          By clicking on the Start button below, you agree that you are 18 years of age or older, have read the information above and are participating voluntarily.
+          {t('setup.agreement')}
         </p>
         <p className='py-2 text-base font-bold'>
-          Note: You will need to be connected to the internet to begin the process. Once running, continuous connection is required.
+          {t('setup.note')}
         </p>
       </div>
     )
--- src/translations.js
+++ src/translations.js
@@ -1,28 +1,66 @@
 const translations = {
   en: {
     translation: {
+      close:"Close",
+      confirmation: {
+        code: "Please enter your verification code",
+        noVerfication:"I did not receive a verification code",
+      },
       dashboard: {
-        welcome: "The Insights Agent is now running. By closing this window, you will not pause the study."
+        welcome: "The Insights Agent is now running. By closing this window, you will not pause the study.",
+        contact: "If you are having trouble or have any questions, please contact tech4all@buildjustly.org.",
+        optout: "If you would like to opt out of this study, click the Pause button in the taskbar and follow instructions.",
+      },
+      next:"Next",
+      send: {
+        number: "Please enter your phone number:",
+      },
+      setup: {
+        before: "Before you click Start, please read this.",
+        purpose: "The purpose of this study is to collect data on how you use your computer.",
+        privacy: "The data will be anonymized to ensure your privacy.",
+        paid: "You will be paid for taking part in this study.",
+        agreement: "By clicking on the Start button below, you agree that you are 18 years of age or older, have read the information above and are participating voluntarily.",
+        note: "Note: You will need to be connected to the internet to begin the process. Once running, continuous connection is required.",
       },
       menu: {
-        dashboard: 'Dashboard',
-        quit: 'Quit',
-        clear: 'Clear Data & Quit',
-      }
-    }
+        quit: "Quit",
+        clear: "Reset",
+        dashboard: "Dashboard",
+      },
+    },
   },
   es: {
     translation: {
+      close:"Cerrar",
+      confirmation: {
+        code: "Por favor ingresa tu código de verificación:",
+        noVerfication:"No recibí un código de verificación",
+      },
       dashboard: {
-        welcome: "La aplicación ahora se está ejecutando. Al cerrar esta ventana, no pausará el estudio."
+        welcome: "Insights Agents se está ejecutando ahora. Si cierra esta ventana, no se pausará el estudio.",
+        contact: "Si tiene problemas o tiene alguna pregunta, envíenos un correo electrónico a tech4all@buildjustly.org",
+        optout: "Si desea optar por no participar en este estudio, haga clic en el botón Detener en la barra de tareas y siga las instrucciones.",
+      },
+      next:"Siguiente",
+      send: {
+        number: "Por favor, introduce tu número de teléfono:",
+      },
+      setup: {
+        before: "Antes de hacer clic en Inicio, lea esto.",
+        purpose: "El propósito de este estudio es recopilar datos sobre cómo usas tu computador.",
+        privacy: "No se recopilarán datos de su información de identificación personal para garantizar su privacidad.",
+        paid: "Se te pagará por hacer parte de este estudio.",
+        agreement: "Al hacer clic en el botón Iniciar de abajo, estás aceptando que tienes 18 años o más, has leído la información anterior y participas voluntariamente.",
+        note: "Nota: Necesitará conexión a Internet para comenzar el proceso. Una vez inicie el proceso, se requiere una conexión continua.",
       },
       menu: {
-        dashboard: 'Panel',
-        quit: 'Salir',
-        clear: 'Borrar',
-      }
+        quit: "Salir",
+        clear: "Reiniciar",
+        dashboard: "Panel de control",
+      },
     }
   },
-}
+};
 
-export default translations
+export default translations;
