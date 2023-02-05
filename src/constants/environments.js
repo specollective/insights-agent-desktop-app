9cc4df901399b40e7d855c4df1a1e2f60806dba0
Joe Torreggiani <joetorreggiani@gmail.com>
2023-02-04 23:45:40 -0800

    Fix dev env constant.

--- src/constants/environments.js
+++ src/constants/environments.js
@@ -1,4 +1,2 @@
-export const NODE_ENV = process.env.NODE_ENV;
-export const DEVELOPMENT_MODE = process.env.DEVELOPMENT === 'true' ||
-  NODE_ENV === 'development' ||
-  NODE_ENV === 'test'
+export const NODE_ENV = process.env.NODE_ENV
+export const DEVELOPMENT_MODE = process.env.DEVELOPMENT === 'true' || NODE_ENV === 'test'
