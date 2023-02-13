e8bae863d49d4a05f8d0dc378230766f82282c7c
Joe Torreggiani <joetorreggiani@gmail.com>
2023-02-13 00:07:16 -0500

    Add intermediate loading state (#91)

--- src/renderer/components/pages/DashboardPage.jsx
+++ src/renderer/components/pages/DashboardPage.jsx
@@ -1,38 +1,28 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import dashboardStyles from '../styles/dashboard';
-import { useTranslation } from 'react-i18next';
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+
 
 function DashboardPage() {
   const { t } = useTranslation()
   const navigate = useNavigate()
 
   useEffect(() => {
-    window.api.onMainNavigation((routeName) => {
-      navigate(routeName)
-    })
+    window.api.onMainNavigation((routeName) => navigate(routeName))
   }, [])
-
-  window.addEventListener('online', () => console.log('Became online'))
-  window.addEventListener('offline', () => console.log('Became offline'))
 
   return (
     <>
       <section className='grid place-content-center h-screen text-lg'>
-        <p>
-          {t('dashboard.welcome')}
-        </p><br />
+        <p className="block mb-4">{ t('dashboard.welcome') }</p>
+        <p className="block mb-4">{ t('dashboard.contact') }</p>
+        <p className="block mb-4">{ t('dashboard.optout') }</p>
 
-        <p>
-          {t('dashboard.contact')}
-        </p><br />
-          {t('dashboard.optout')}
-        <p>
-        </p><br />
         <div className='float-right pt-8 text-right'>
           <button className="p-2 text-xl underline w-40" onClick={window.api.hideApp}>
             {t('close')}
           </button>
+
           <button 
             className="rounded p-2 h-11 w-40 bg-[#70B443] text-xl font-semibold active:bg-green-600"
             onClick={window.api.downloadData}
